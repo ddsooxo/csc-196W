7a0be3ad3b60018cc37a630f06d9111d0bea7408
ddsooxo <dchuteng@gmail.com>
2021-03-04 23:50:51 -0800

    worked on some DJS DOM Manipulation exercises, has errors, will fix tomorrow

--- wk5/index.js
+++ wk5/index.js
@@ -1,3 +1,26 @@
-const h1 = document.getElementsByTagName('h1')[0];
-const p = document.getElementsByTagName('p')[0];
-const ul = document.getElementsByTagName('ul')[0];
+// const h1 = document.getElementsByTagName('h1')[0];
+// const p = document.getElementsByTagName('p')[0];
+// const ul = document.getElementsByTagName('ul')[0];
+// var text = document.getElementById("main-title").innerHTML;
+// alert("the main title is" + text);
+
+
+//grab the HTML element and store it in the variable
+
+function firstDiv() {
+    var output = document.getElementById("firstDiv").innerHTML =
+        'The content of the first div is ' + document.getElementById()
+    console.log(output)
+
+    //alert(output);
+}
+
+
+function tagName1() {
+    var p = document.getElementsByTagName('p');
+}
+
+function tagName2() {
+    // selects the p tag that resides only under div id 'first-div'
+    var p = first - div.getElementsByTagName('p');
+}
